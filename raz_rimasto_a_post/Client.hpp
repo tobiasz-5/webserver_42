03fa04d0ab39c7d200eea88271c98559bf3b3696
Razvan Paic <rpaic@c2r4p9.42firenze.it>
2025-07-05 21:56:23 +0200

    raz rimasto a post

---
+++ raz_rimasto_a_post/Client.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <map>
+#include "Request.hpp"
+#include "Server.hpp"
+
+class Client
+{
+    private:
+        int fd;
+        struct sockaddr_in addr;
+        std::string response;
+        const Server *accepted_server; // server a cui è connesso il client
+        Request request;
+    public:
+        Client();
+        Client(int fd, struct sockaddr_in addr, const Server *accepted_server);
+        Client(Client const &other);
+		Client &operator=(Client const &other);
+        ~Client();
+
+        const int &getClientfd(void) const;
+        const Server *getServer(void) const;
+        const struct sockaddr_in &getStructaddr(void) const;
+        const Request &getRequest(void) const;
+        const std::string &getresponse(void) const;
+
+        void set_response(std::string s);
+        void clearRequest(); // Clears the request data for the next request
+
+        int receiveRequest();
+};
---
+++ raz_rimasto_a_post/manage_request.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "Server.hpp"
+#include "Client.hpp"
+
+void set_response_for_client(Client &client);
